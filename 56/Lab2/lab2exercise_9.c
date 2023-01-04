8a00330920e581f179107aeec9ed96ac4df4042f
Brook09 <121922500+Brook09@users.noreply.github.com>
2023-01-04 14:55:30 +0700

    .

---
+++ 56/Lab2/lab2exercise_9.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int A = 0, B = 0, M = 0, N = 0;
+    scanf("%d %d %d %d", &A, &B, &M, &N);
+    if ((A == 0 && M > 0) || (B == 0 && N > 0))
+    {
+        printf("Unable to finish order");
+    }
+    else
+    {
+        int r1, r2;
+        if (A == 0 && M == 0)
+        {
+            r1 = 0;
+        }
+        else
+        {
+            r1 = ceil(M * 1.0 / (A * 6.0));
+        }
+
+        if (M == 0 && N == 0)
+        {
+            r2 = 0;
+        }
+        else
+        {
+            r2 = ceil(N * 1.0 / (B * 10.0));
+        }
+        if (r1 > r2)
+        {
+            printf("%d", r1);
+        }
+        else
+        {
+            printf("%d", r2);
+        }
+    }
+    return 0;
+}
